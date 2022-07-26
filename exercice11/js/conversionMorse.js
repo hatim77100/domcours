7374ea7bc4831b0aec71a12ebb77b44301198a9d
hatim77100 <najoua-hatim@hotmail.fr>
2022-07-26 23:19:32 +0200

    update js

---
+++ exercice11/js/conversionMorse.js
@@ -0,0 +1,43 @@
+
+
+encodeMessage("bonjour le monde");
+function encodeMessage(msg){
+var msgEncode = "";
+for(var i = 0 ; i < msg.length ; i++){
+    msgEncode += lettreAMorse(msg[i]) ;
+}
+return msgEncode
+}
+
+function lettreAMorse(lettre){
+    switch(lettre.toLowerCase()){
+        case 'a' : return ".-";
+        case 'b' : return "-...";
+        case 'c' : return "-.-.";
+        case 'd' : return "-..";
+        case 'e' : return ".";
+        case 'f' : return "..-.";
+        case 'g' : return "--.";
+        case 'h' : return "....";
+        case 'i' : return "..";
+        case 'j' : return ".---";
+        case 'k' : return "-.-";
+        case 'l' : return ".-..";
+        case 'm' : return "--";
+        case 'n' : return "-.";
+        case 'o' : return "---";
+        case 'p' : return ".--.";
+        case 'q' : return "--.-";
+        case 'r' : return ".-.";
+        case 's' : return "...";
+        case 't' : return "..-";
+        case 'u' : return "...-";
+        case 'v' : return ".--";
+        case 'w' : return "-..-";
+        case 'x' : return "-..-";
+        case 'y' : return "-.--";
+        case 'z' : return "--..";
+        case ' ' : return ' '; 
+    default      : return "";
+    }
+}
