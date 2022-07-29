322d40b827bacc0f4eacf5a11b45cf17912cc3a4
hatim77100 <najoua-hatim@hotmail.fr>
2022-07-29 22:29:58 +0200

    end exercice

--- sujet+Exercice+12/main.js
+++ sujet+Exercice+12/main.js
@@ -1,32 +1,54 @@
-var number1 = document.querySelector("#nb1").value;
-var number2 = document.querySelector("#nb2").value;
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-document.getElementsByClassName("resultat").value = addition(number1, number2)
+  var number1 = parseInt(document.getElementById("nb1").value);
+  var number2 = parseInt(document.getElementById("nb2").value);
+  var operation = document.querySelector("#operation").value;
 
-function addition(value1,value2){
-    var nombre1 = parseInt(value1);
-    var nombre2= parseInt(value2);
-    result = nombre1 + nombre2;
-    return result;
-}
+  var resultat = 0;
+  switch(operation) {
+    case "+":
+      resultat = number1 + number2;
+      break;
+    case "-":
+      resultat = number1 - number2;
+      break;
+    case "*":
+      resultat = number1 * number2;
+      break;
+    case "/":
+      resultat = number1 / number2;
+      break;
+    default:
+      "Erreur de calcul";
+  }
+  document.querySelector(".resultat").textContent = resultat;
+});
 
-function soustraction(value1,value2){
-    var nombre1 = parseInt(value1);
-    var nombre2= parseInt(value2);
-    result = nombre1 - nombre2;
-    return result;
-}
+// function addition(value1,value2){
+//     var nombre1 = parseInt(value1);
+//     var nombre2= parseInt(value2);
+//     var result = nombre1 + nombre2;
+//     return result;
+// }
 
-function multiplication(value1,value2){
-    var nombre1 = parseInt(value1);
-    var nombre2= parseInt(value2);
-    result = nombre1 * nombre2;
-    return result;
-}
+// function soustraction(value1,value2){
+//     var nombre1 = parseInt(value1);
+//     var nombre2= parseInt(value2);
+//     var result = nombre1 - nombre2;
+//     return result;
+// }
 
-function division(value1,value2){
-    var nombre1 = parseInt(value1);
-    var nombre2= parseInt(value2);
-    result = nombre1 / nombre2;
-    return result;
-}
+// function multiplication(value1,value2){
+//     var nombre1 = parseInt(value1);
+//     var nombre2= parseInt(value2);
+//     var result = nombre1 * nombre2;
+//     return result;
+// }
+
+// function division(value1,value2){
+//     var nombre1 = parseInt(value1);
+//     var nombre2= parseInt(value2);
+//     var result = nombre1 / nombre2;
+//     return result;
+// }
